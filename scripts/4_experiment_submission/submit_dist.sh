c85e6cb0349b9cc5b0aedf3c253b1c8c3126747f
Colby Ford <colbytylerford@gmail.com>
2024-06-03 18:05:26 -0400

    Update submit_dist.sh

--- scripts/4_experiment_submission/submit_dist.sh
+++ scripts/4_experiment_submission/submit_dist.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=cford_haddock3_h5_dist
-#SBATCH --partition=Orion
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=36
-#SBATCH --constraint=skylake
-#SBATCH --mem=300gb
-#SBATCH --time=5:00:00
+##SBATCH --job-name=cford_haddock3_h5_dist
+##SBATCH --partition=Orion
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=36
+##SBATCH --constraint=skylake
+##SBATCH --mem=300gb
+##SBATCH --time=5:00:00
 
 export input_csv=experiments_test.csv
 # export input_csv=experiments_full.csv
 
+echo "Submitting HADDOCK 3 Experiments..."
+echo "==================================="
 while IFS="," read -r experiment_id
 do
-  echo "Submitting HADDOCK 3 Experiment..."
   echo "  Experiment ID: $experiment_id"
-  echo "=================================="
-  JOBID=$(bash submit_iter.sh $experiment_id)
+  echo "==================================="
+  JOBID=$(sbatch --parsable submit_iter.sh $experiment_id)
 done < <(tail -n +2 $input_csv)
