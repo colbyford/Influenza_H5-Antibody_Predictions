cfef3e86d51c9262bbeb217cd95f772b3361ecf7
Colby Ford <colbytylerford@gmail.com>
2024-06-03 17:21:42 -0400

    Update submit_dist.sh

--- scripts/4_experiment_submission/submit_dist.sh
+++ scripts/4_experiment_submission/submit_dist.sh
@@ -7,19 +7,13 @@
 #SBATCH --mem=300gb
 #SBATCH --time=5:00:00
 
-export SINGULARITY_CONTAINER_HOME=/home/gridsan/$USER/seqer_shared/N_cytokine_docking
-export input_csv=../data/experiment/experiment_list.csv
+export input_csv=experiments_test.csv
+# export input_csv=experiments_full.csv
 
-# module load singularity
-# singularity run $SINGULARITY_CONTAINER_HOME/haddock.sif
-
-while IFS="," read -r complex_id experiment_path n_protein n_pdb n_residues cytokine_protein cytokine_pdb cytokine_residues
+while IFS="," read -r experiment_id
 do
-  echo "Complex ID: $complex_id"
-  echo "Experiment Path: $experiment_path"
-  echo ""
-
-  # JOBID=$(sbatch --parsable submit_iter.sh $complex_id $experiment_path)
-  JOBID=$(bash submit_iter.sh $complex_id $experiment_path)
-
+  echo "Submitting HADDOCK 3 Experiment..."
+  echo "  Experiment ID: $experiment_id"
+  echo "=================================="
+  JOBID=$(bash submit_iter.sh $experiment_id)
 done < <(tail -n +2 $input_csv)
