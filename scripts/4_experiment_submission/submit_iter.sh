eef2a757fe422458fdbe2793f9bc600e1a4307b9
Colby Ford <colbytylerford@gmail.com>
2024-06-03 18:05:30 -0400

    Update submit_iter.sh

--- scripts/4_experiment_submission/submit_iter.sh
+++ scripts/4_experiment_submission/submit_iter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=cford_haddock3_h5_$1
+#SBATCH --job-name=cford_h5_${1}
 #SBATCH --partition=Orion
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=36
-#SBATCH --constraint=skylake
-#SBATCH --mem=300gb
-#SBATCH --time=5:00:00
+##SBATCH --constraint=skylake
+#SBATCH --mem=32gb
+#SBATCH --time=6:00:00
 
 module load singularity
 export SINGULARITY_CONTAINER_HOME=$(pwd)
 
 cd ../../data/experiments/$1
-singularity exec $SINGULARITY_CONTAINER_HOME/haddock3.sif "haddock3 config.cfg"
+singularity exec $SINGULARITY_CONTAINER_HOME/haddock3.sif haddock3 config.cfg
