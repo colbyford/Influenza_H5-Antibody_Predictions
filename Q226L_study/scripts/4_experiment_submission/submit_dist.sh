2e088be3e8d6295be843491f5ae46b2b4eff6a8c
Colby Ford <colbytylerford@gmail.com>
2024-12-13 14:33:21 -0500

    Add HADDOCK3 experiments

---
+++ Q226L_study/scripts/4_experiment_submission/submit_dist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##SBATCH --job-name=cford_haddock3_h5_dist
+##SBATCH --partition=Nebula
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=36
+##SBATCH --constraint=skylake
+##SBATCH --mem=300gb
+##SBATCH --time=5:00:00
+
+# export input_csv=experiments_test.csv
+export input_csv=experiments_full.csv
+# export input_csv=experiments_part_0.csv
+# export input_csv=experiments_part_1.csv
+
+echo "Submitting HADDOCK 3 Experiments..."
+echo "==================================="
+while IFS="," read -r experiment_id
+do
+  echo "  Experiment ID: $experiment_id"
+  echo "==================================="
+  JOBID=$(sbatch --parsable submit_iter.sh $experiment_id)
+done < <(tail -n +2 $input_csv)
---
+++ Q226L_study/scripts/4_experiment_submission/submit_iter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name="hdk3_h5_fab"
+##SBATCH --partition=Pisces
+#SBATCH --partition=Draco
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --mem=32gb
+#SBATCH --time=1:00:00
+
+module load singularity
+export SINGULARITY_CONTAINER_HOME=$(pwd)
+
+# cd ../../data/experiments/$1
+cd /scratch/cford38/Influenza_H5-Antibody_Predictions/Q226L_study/data/experiments/$1
+singularity exec $SINGULARITY_CONTAINER_HOME/haddock3.sif haddock3 config.cfg
